e8705f5e9e1ef83e3286f592e46df64863467b4b
paul4589 <paul4589@gmailcom>
2017-11-01 08:36:52 +1100

    Third commit

--- script.js
+++ script.js
@@ -49,7 +49,7 @@
     // string object's 'toLowerCase' method on the result so we can compare
     // to lower case character 'j' afterwards.
     // Look up these methods on Mozilla Developer Network web site if needed.
-     var firstLetter = names[i].charAt(0).toLowerCase();
+     var firstLetter = names[i].charAt(0).toLowerCase().charAt(0);
 
     // STEP 12:
     // Compare the 'firstLetter' retrieved in STEP 11 to lower case
